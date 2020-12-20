b3cd9be0ccb57f63ec314b5f8d801255e1e788ce
Dmitry Spitsyn <dvspicin@yandex.ru>
2020-12-20 18:58:05 +0500

    Работа на ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,33 @@
-console.log('hello');
 let content = document.querySelector('.content');
 let editbutton = content.querySelector('.profile__edit-button');
-let overlay = content.querySelector('.overlay');
-let CloseButton = overlay.querySelector('.form__button-reset');
-let submit = overlay.querySelector('.form');
+let popup = content.querySelector('.popup');
+let CloseButton = popup.querySelector('.form__button-reset');
+let submit = popup.querySelector('.form');
 let profiletitle = content.querySelector('.profile__title');
 let profilesubtitle = content.querySelector('.profile__subtitle');
-let formName = overlay.querySelector('.form__input-name');
-let formdescription = overlay.querySelector('.form__input-description');
+let formName = popup.querySelector('.form__input-name');
+let formdescription = popup.querySelector('.form__input-description');
+
 
 
 editbutton.addEventListener('click', OpenPopUp);
-overlay.addEventListener('click', ClosePopup);
+popup.addEventListener('click', ClosePopup);
 submit.addEventListener('submit', SubmitForm);
 
 
+
 function OpenPopUp() {
-    console.log(profilesubtitle.textContent)
+
     formName.value = profiletitle.textContent;
     formdescription.value = profilesubtitle.textContent;
-    overlay.classList.add('overlay__visible_on');
+    popup.classList.add('popup_opened');
 
 }
 
 function ClosePopup() {
-    console.log(event.target);
-    if (event.target === overlay || event.target === CloseButton) {
-        overlay.classList.remove('overlay__visible_on');
+
+    if (event.target === popup || event.target === CloseButton) {
+        popup.classList.remove('popup_opened');
 
     }
 }
@@ -35,8 +36,5 @@ function SubmitForm(event) {
     event.preventDefault();
     profiletitle.textContent = formName.value;
     profilesubtitle.textContent = formdescription.value;
-    overlay.classList.remove('overlay__visible_on');
-
-
-
+    popup.classList.remove('popup_opened');
 }
