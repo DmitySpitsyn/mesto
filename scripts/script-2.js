f893e506371319573b88f2c475e517a5b4a70848
Dmitry Spitsyn <dvspicin@yandex.ru>
2021-01-18 23:09:13 +0500

    finished 90%

---
+++ scripts/script-2.js
@@ -0,0 +1,84 @@
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+const sectionElements = document.querySelector(".section-elements").content;
+const Elements = document.querySelector(".elements");
+const AddElement = document.querySelector('.profile__add-button');
+const formtitle = document.querySelector('.container__title');
+const imagefull = content.querySelector('.container__image');
+const containerForm = content.querySelector('.popup__form');
+const containerImage = content.querySelector('.popup__image');
+const image = content.querySelector('.container__image');
+const CloseImage = popup.querySelector('.popup__image_close');
+AddElement.addEventListener('click', AddItem);
+render();
+
+function render() {
+
+    initialCards.forEach(renderItem);
+}
+
+function renderItem(text) {
+    const htmlElement = sectionElements.cloneNode(true);
+
+    htmlElement.querySelector('.element__image').src = text.link;
+    htmlElement.querySelector('.element__title').textContent = text.name;
+    htmlElement.querySelector('.element__like-button').addEventListener('click', buttonLikeSwith);
+    htmlElement.querySelector('.element__delete-button').addEventListener('click', ItemDelete);
+    htmlElement.querySelector('.element__image').addEventListener('click', popupImage);
+    Elements.appendChild(htmlElement);
+}
+
+
+function buttonLikeSwith(evt) {
+
+    evt.target.closest('.element__like-button').classList.toggle('element__like-button_active');
+}
+
+function ItemDelete(evt) {
+    evt.target.closest('.element').remove();
+    console.log(evt.target);
+
+}
+
+function AddItem() {
+    OpenPopUp();
+    containerImage.classList.remove('container_visible_on');
+    containerForm.classList.add('container_visible_on');
+    formName.value = '';
+    formName.placeholder = 'Название';
+    formdescription.value = '';
+    formdescription.placeholder = 'Ссылка на картинку';
+    formtitle.textContent = 'Новое место';
+}
+
+function popupImage(evt) {
+    OpenPopUp();
+    containerImage.classList.add('container_visible_on');
+    containerForm.classList.remove('container_visible_on');
+    image.src = evt.target.src;
+    console.log(evt.target.src);
+
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 let content = document.querySelector('.content');
 let editbutton = content.querySelector('.profile__edit-button');
 let popup = content.querySelector('.popup');
-let CloseButton = popup.querySelector('.container__button-close');
+let CloseButton = popup.querySelector('.popup__form_close');
 let submit = popup.querySelector('.form');
 let profiletitle = content.querySelector('.profile__title');
 let profilesubtitle = content.querySelector('.profile__subtitle');
@@ -10,7 +10,7 @@ let formdescription = popup.querySelector('.form__input_type_description');
 
 
 
-editbutton.addEventListener('click', OpenPopUp);
+editbutton.addEventListener('click', OpenForm);
 popup.addEventListener('click', ClosePopup);
 submit.addEventListener('submit', SubmitForm);
 
@@ -18,23 +18,42 @@ submit.addEventListener('submit', SubmitForm);
 
 function OpenPopUp() {
 
-    formName.value = profiletitle.textContent;
-    formdescription.value = profilesubtitle.textContent;
     popup.classList.add('popup_opened');
 
 }
 
 function ClosePopup() {
 
-    if (event.target === popup || event.target === CloseButton) {
+    if (event.target === popup || event.target === CloseButton || event.target === CloseImage) {
         popup.classList.remove('popup_opened');
 
     }
 }
 
+function OpenForm() {
+    OpenPopUp();
+    containerImage.classList.remove('container_visible_on');
+    containerForm.classList.add('container_visible_on');
+    formName.value = profiletitle.textContent;
+    formdescription.value = profilesubtitle.textContent;
+    formtitle.textContent = 'Редактировать профиль';
+}
+
 function SubmitForm(event) {
     event.preventDefault();
-    profiletitle.textContent = formName.value;
-    profilesubtitle.textContent = formdescription.value;
+    if (formtitle.textContent === 'Редактировать профиль') {
+        profiletitle.textContent = formName.value;
+        profilesubtitle.textContent = formdescription.value;
+    }
+    if (formtitle.textContent === 'Новое место') {
+
+        const elements = document.querySelector('.elements');
+        while (elements.firstChild) {
+            elements.removeChild(elements.firstChild);
+        }
+        initialCards.splice(0, 0, { name: formName.value, link: formdescription.value });
+
+        render();
+    }
     popup.classList.remove('popup_opened');
 }
